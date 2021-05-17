468cef4e60f819bd1ec79322e02ef0178a5de93d
Nehla <nehlajaleel35@gmail.com>
2021-05-17 15:23:50 +0530

    commited again

--- Mydoc.java
+++ Mydoc.java
@@ -1,6 +1,6 @@
 import java.io.*;
-class Mydoc{
-public static void main(String args[])
+public class Mydoc{
+public static void main(String[] args)
 {
  System.out.println("Git Jenkins Integration");
 }
